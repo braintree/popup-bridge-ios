53ec402f5f7233995a317f2b08115b3ecefe977a
Sammy Cannillo <sammyjaynecannillo@gmail.com>
2023-02-10 12:27:42 -0600

    Update POPPopupBridgeSwift to use POPPopupBridgeDelegateSwift + more swift refactor

--- Sources/PopupBridge/POPPopupBridge.swift
+++ Sources/PopupBridge/POPPopupBridge.swift
@@ -1,7 +1,7 @@
 import Foundation
 import SafariServices
 
-class POPPopupBridgeSwift: NSObject, WKScriptMessageHandler, SFSafariViewControllerDelegate {
+public class POPPopupBridgeSwift: NSObject, WKScriptMessageHandler, SFSafariViewControllerDelegate {
     
     // Constants
     let kPOPScriptMessageHandlerName = "POPPopupBridge"
@@ -9,14 +9,14 @@ class POPPopupBridgeSwift: NSObject, WKScriptMessageHandler, SFSafariViewControl
     
     // Properties
     let webView: WKWebView
-    let delegate: POPPopupBridgeDelegate
+    let delegate: POPPopupBridgeDelegateSwift
     var scheme: String?
     var safariViewController: SFSafariViewController?
     
-    private var returnBlock: ((URL?) -> Bool)?
+    private static var returnBlock: ((URL?) -> Bool)?
     
     // TODO: - make unfailable
-    init?(webView: WKWebView, delegate: POPPopupBridgeDelegate) {
+    init?(webView: WKWebView, delegate: POPPopupBridgeDelegateSwift) {
         guard let scheme = scheme else {
             // TODO: - Raise exception
             return nil
@@ -24,7 +24,7 @@ class POPPopupBridgeSwift: NSObject, WKScriptMessageHandler, SFSafariViewControl
         
         // Step 1 - get merchant's webView
         self.webView = webView
-        self.delegate = delegate
+        self.delegate = delegate // merchant implements POPUpBridgeDelegate
         
         super.init() // do we need this?
         
@@ -46,7 +46,7 @@ class POPPopupBridgeSwift: NSObject, WKScriptMessageHandler, SFSafariViewControl
         webView.configuration.userContentController.addUserScript(script)
         
 //        weak var weakSelf = self
-        returnBlock = { (url : URL?) -> Bool in
+        POPPopupBridgeSwift.returnBlock = { (url : URL?) -> Bool in
             let error = "null"
             let payload = "null"
             let script: String
@@ -55,11 +55,42 @@ class POPPopupBridgeSwift: NSObject, WKScriptMessageHandler, SFSafariViewControl
                 let urlComponents = URLComponents.init(url: url, resolvingAgainstBaseURL: false)
                 let path = urlComponents?.path
     
-                // TODO: - finish
+                if (urlComponents?.scheme?.localizedCaseInsensitiveCompare(scheme) != ComparisonResult.orderedSame ||
+                    urlComponents?.host?.localizedCaseInsensitiveCompare(self.kPOPURLHost) != ComparisonResult.orderedSame) {
+                    return false
+                }
+                
+                self.dismissSafariViewController()
+                
+                var payloadDictionary: [String: Any] = [:]
+                payloadDictionary["path"] = path
+                payloadDictionary["queryItems"] = self.dictionaryFor(queryString: url.query!)
+                if let fragment = url.fragment {
+                    payloadDictionary["hash"] = fragment
+                }
+                
+                do {
+                    let payloadData = try JSONSerialization.data(withJSONObject: payloadDictionary)
+                    let payload = String(data: payloadData, encoding: .utf8)!
+                    
+                    script = "window.popupBridge.onComplete(null, \(payload);"
+                } catch {
+                    script = "window.popupBridge.onComplete(null, null);"
+                }
+
+            } else {
+                script = """
+                if (typeof window.popupBridge.onCancel === 'function') {\
+                  window.popupBridge.onCancel();\
+                } else {\
+                  window.popupBridge.onComplete(null, null);\
+                }
+                """
             }
+            
+            self.injectWebView(webView: webView, withJavaScript: script)
             return true
         }
-        
     }
     
     func setReturlURLScheme(returnURLScheme: String) {
@@ -69,32 +100,61 @@ class POPPopupBridgeSwift: NSObject, WKScriptMessageHandler, SFSafariViewControl
     let javascriptTemplate = "        ;(function () {            if (!window.popupBridge) { window.popupBridge = {}; };                        window.popupBridge.getReturnUrlPrefix = function getReturnUrlPrefix() {                return '%%SCHEME%%://%%HOST%%/';            };                        window.popupBridge.open = function open(url) {                window.webkit.messageHandlers.%%SCRIPT_MESSAGE_HANDLER_NAME%%.postMessage({                    url: url                });            };                        window.popupBridge.sendMessage = function sendMessage(message, data) {                window.webkit.messageHandlers.%%SCRIPT_MESSAGE_HANDLER_NAME%%.postMessage({                    message: {                        name: message,                        data: data                    }                });            };                        return 0;        })();"
 
     static func openURL(url: URL, sourceApplication: String) -> Bool {
-        return true
+        return POPPopupBridgeSwift.openURL(url: url)
     }
     
     static func openURL(url: URL, options: [UIApplication.OpenURLOptionsKey: Any]) -> Bool {
-        return true
+        return POPPopupBridgeSwift.openURL(url: url)
     }
     
     static func openURL(url: URL) -> Bool {
-        return true
+        if let returnBlock {
+            return returnBlock(url)
+        } else {
+            return false
+        }
     }
     
     func dismissSafariViewController() {
-        //
+        if let safariViewController {
+            if delegate.responds(to: #selector(POPPopupBridgeDelegate.popupBridge(_:requestsDismissalOf:))) {
+                delegate.popupBridge(self, requestsDismissalOfViewController: safariViewController)
+            }
+        }
     }
     
     // MARK: - SFSafariViewControllerDelegate
     
-    func safariViewControllerDidFinish(_ controller: SFSafariViewController) {
+    public func safariViewControllerDidFinish(_ controller: SFSafariViewController) {
         //
     }
     
     // MARK: - WKScriptMessageHandler conformance
     // Receives messages from JS
     
-    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
-        // TODO
+    public func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+        if message.name == kPOPScriptMessageHandlerName {
+            let params = message.body as! [String: Any]
+            let urlString = params["url"] as! String?
+            if urlString != nil {
+                dismissSafariViewController()
+                
+                let url = URL(string: urlString!)
+                
+                // do we need to do this responds to selector thing?
+                if delegate.responds(to: #selector(POPPopupBridgeDelegate.popupBridge(_:willOpen:))) {
+                    delegate.popupBridge?(self, willOpenURL: url!)
+                }
+                return
+            }
+            
+            // TODO - this parsing is awful
+            if let name = (params as? [String: [String: String]])?["message"]?["name"],
+               delegate.responds(to: #selector(POPPopupBridgeDelegate.popupBridge(_:receivedMessage:data:))) {
+                let data = (params as? [String: [String: String]])?["message"]?["data"]
+                delegate.popupBridge?(self, receivedMessage: name, data: data)
+            }
+        }
     }
     
     // MARK: - Helpers
---
+++ Sources/PopupBridge/POPPopupBridgeDelegateSwift.swift
@@ -0,0 +1,22 @@
+import Foundation
+
+/// Set a delegate to handle various events the "popup" lifecycle.
+@objc public protocol POPPopupBridgeDelegateSwift : NSObjectProtocol {
+
+    
+    /// Popup Bridge will provide a Safari View Controller to your delegate.
+    /// You must present the view controller in this method.
+    @objc func popupBridge(_ bridge: POPPopupBridgeSwift, requestsPresentationOfViewController viewController: Any!)
+
+    
+    /// You must dismiss the view controller in this method.
+    @objc func popupBridge(_ bridge: POPPopupBridgeSwift, requestsDismissalOfViewController viewController: Any!)
+
+    
+    /// Optional: Receive the URL that will be opened
+    @objc optional func popupBridge(_ bridge: POPPopupBridgeSwift, willOpenURL url: URL)
+
+    
+    /// Optional: Receive messages from the web view
+    @objc optional func popupBridge(_ bridge: POPPopupBridgeSwift, receivedMessage messageName: String, data: String?)
+}
