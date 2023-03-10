ac736da60531ff8383eb90b4aafac50cebf067f5
Sammy Cannillo <scannillo@paypal.com>
2023-03-10 15:07:38 -0600

    Update import instructions in README

--- UnitTests/MockPopupBridgeDelegate.swift
+++ UnitTests/MockPopupBridgeDelegate.swift
@@ -2,16 +2,16 @@ import UIKit
 import PopupBridge
 
 // TODO: - Can we get rid of NSObj requirement
-public class DelegateImplementation: NSObject, POPPopupBridgeDelegate {
+public class MockPopupBridgeDelegate: NSObject, POPPopupBridgeDelegate {
     
     override init() { }
     
     public func popupBridge(_ bridge: PopupBridge.POPPopupBridge, requestsPresentationOfViewController viewController: UIViewController) {
-        // fake implementation
+        //
     }
     
     public func popupBridge(_ bridge: PopupBridge.POPPopupBridge, requestsDismissalOfViewController viewController: UIViewController) {
-        // fake implementation
+        //
     }
     
 }
