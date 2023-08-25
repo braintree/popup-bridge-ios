0eced4df1c10c34d021f2236231f0cb1ced1d5b5
Joon Hong <joon@getkinn.com>
2023-08-25 11:16:06 -0400

    Prevent retain cycle from closure capture

--- Sources/PopupBridge/POPPopupBridge.swift
+++ Sources/PopupBridge/POPPopupBridge.swift
@@ -29,12 +29,12 @@ public class POPPopupBridge: NSObject, WKScriptMessageHandler {
 
         configureWebView()
                 
-        returnBlock = { url in
-            guard let script = self.constructJavaScriptCompletionResult(returnURL: url) else {
+        returnBlock = { [weak self] url in
+            guard let script = self?.constructJavaScriptCompletionResult(returnURL: url) else {
                 return
             }
 
-            self.injectWebView(webView: webView, withJavaScript: script)
+            self?.injectWebView(webView: webView, withJavaScript: script)
             return
         }
     }
