86188b3697b01a91f919f65009d26aa7066c3d9a
richherrera <ricardherrera@paypal.com>
2025-02-20 12:48:30 -0600

    Add PopupBridgeAnalytics enum

---
+++ Sources/PopupBridge/PopupBridgeAnalytics.swift
@@ -0,0 +1,8 @@
+enum PopupBridgeAnalytics {
+    
+    static let started = "popup-bridge:started"
+    static let succeeded = "popup-bridge:succeeded"
+    static let failed = "popup-bridge:failed"
+    static let canceled = "popup-bridge:canceled"
+}
+
