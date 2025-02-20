a9ef086899560c1a19cef71daf70d597f6eecd77
Rich Herrera <ricardherrera@paypal.com>
2025-02-20 12:50:50 -0600

    [V3] Add FPTIBatchData model for events (#72)
    * Add FPTIBatchData structure
    
    * Keys sorted by name
    
    * Add Bundle extension to expose BundleShortVersion
    
    * Define clientSDKVersion tag

---
+++ Sources/PopupBridge/FPTIBatchData.swift
@@ -0,0 +1,78 @@
+import UIKit
+
+struct FPTIBatchData: Codable {
+    
+    let appID: String = Bundle.main.infoDictionary?[kCFBundleIdentifierKey as String] as? String ?? "N/A"
+    
+    let appName: String = Bundle.main.infoDictionary?[kCFBundleNameKey as String] as? String ?? "N/A"
+    
+    let clientSDKVersion: String = Bundle.clientSDKVersion
+    
+    let clientOS: String = UIDevice.current.systemName + " " + UIDevice.current.systemVersion
+    
+    let component: String = "popupbridgesdk"
+    
+    let deviceManufacturer: String = "Apple"
+    
+    let deviceModel: String = {
+        var systemInfo = utsname()
+        uname(&systemInfo)
+        let machineMirror = Mirror(reflecting: systemInfo.machine)
+        let identifier = machineMirror.children.reduce("") { identifier, element in
+            guard let value = element.value as? Int8, value != 0 else { return identifier }
+            return identifier + String(UnicodeScalar(UInt8(value)))
+        }
+        return identifier
+    }()
+    
+    let eventName: String
+    
+    let eventSource: String = "mobile-native"
+    
+    let isSimulator: Bool = {
+        #if targetEnvironment(simulator)
+            true
+        #else
+            false
+        #endif
+    }()
+    
+    let merchantAppVersion: String = Bundle.main.infoDictionary?[kCFBundleVersionKey as String] as? String ?? "N/A"
+    
+    let packageManager: String = {
+        #if COCOAPODS
+            "CocoaPods"
+        #elseif SWIFT_PACKAGE
+            "Swift Package Manager"
+        #else
+            "Carthage or Other"
+        #endif
+    }()
+    
+    let platform: String = "iOS"
+    
+    let sessionID: String
+    
+    let timestamp: String = String(Date().utcTimestampMilliseconds)
+    
+    let tenantName: String = "Braintree"
+    
+    enum CodingKeys: String, CodingKey {
+        case appID = "app_id"
+        case appName = "app_name"
+        case clientSDKVersion = "c_sdk_ver"
+        case clientOS = "client_os"
+        case component = "comp"
+        case deviceManufacturer = "device_manufacturer"
+        case deviceModel = "mobile_device_model"
+        case eventName = "event_name"
+        case eventSource = "event_source"
+        case isSimulator = "is_simulator"
+        case merchantAppVersion = "mapv"
+        case packageManager = "ios_package_manager"
+        case platform
+        case sessionID = "session_id"
+        case timestamp = "t"
+        case tenantName = "tenant_name"
+    }
+}
