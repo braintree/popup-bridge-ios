4ed21f39eb2f39d9f38ce95e32cc8b91bd693a4e
Rich Herrera <ricardherrera@paypal.com>
2025-03-18 11:15:43 -0600

    [V3] Add Venmo install check (#78)
    * Add venmo scheme to the allowlist in info.plist.
    
    * Add URLOpenet protocol and uiapplication extension
    
    * Add Venmo installed validation and pass it to the user script.
    
    * Add docstring
    
    * Use internal instead of static
    
    * Remove bool description
    
    * Update CHANGELOG
    
    * Update README
    
    * Update V3 migration guide

--- Sources/PopupBridge/POPPopupBridge.swift
+++ Sources/PopupBridge/POPPopupBridge.swift
@@ -13,6 +13,7 @@ public class POPPopupBridge: NSObject, WKScriptMessageHandler {
     private let messageHandlerName = "POPPopupBridge"
     private let hostName = "popupbridgev1"    
     private let webView: WKWebView
+    private let application: URLOpener = UIApplication.shared
     private var webAuthenticationSession: WebAuthenticationSession = WebAuthenticationSession()
     
     private var returnBlock: ((URL) -> Void)? = nil
@@ -92,7 +93,8 @@ public class POPPopupBridge: NSObject, WKScriptMessageHandler {
         let javascript = PopupBridgeUserScript(
             scheme: PopupBridgeConstants.callbackURLScheme,
             scriptMessageHandlerName: messageHandlerName,
-            host: hostName
+            host: hostName,
+            isVenmoInstalled: application.isVenmoAppInstalled()
         ).rawJavascript
         
         let script = WKUserScript(
