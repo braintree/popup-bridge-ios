81a13428c07682cef927218ccbff2c023eef05a7
richherrera <ricardherrera@paypal.com>
2025-03-24 10:00:44 -0600

    Add trackForMemoryLeaks method

--- UnitTests/XCTestCase+AssertMemoryLeaks.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-import XCTest
-
-extension XCTestCase {
-    
-    func assertMemoryLeak(instance: AnyObject, file: StaticString, line: UInt) {
-        XCTAssertNil(instance, "Expected \(String(describing: instance)) to be deallocated. Potential memory leak!", file: file, line: line)
-    }
-}
