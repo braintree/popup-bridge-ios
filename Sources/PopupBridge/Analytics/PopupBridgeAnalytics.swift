f2f708b6d9a410ce92bcab7f51cf60d03a83212c
Tanya Jain <tanyjain@paypal.com>
2026-03-09 16:47:29 -0500

    Enhanced popup bridge

--- Sources/PopupBridge/Analytics/PopupBridgeAnalytics.swift
+++ Sources/PopupBridge/Analytics/PopupBridgeAnalytics.swift
@@ -1,8 +1,14 @@
 enum PopupBridgeAnalytics {
-    
+
     static let started = "popup-bridge:started"
     static let succeeded = "popup-bridge:succeeded"
     static let failed = "popup-bridge:failed"
     static let canceled = "popup-bridge:canceled"
+
+    static let paypalInstalled = "popup-bridge:app-detection:paypal-installed"
+    static let paypalNotInstalled = "popup-bridge:app-detection:paypal-not-installed"
+    static let appLaunchStarted = "popup-bridge:app-launch:started"
+    static let appLaunchSucceeded = "popup-bridge:app-launch:succeeded"
+    static let appLaunchFailed = "popup-bridge:app-launch:failed"
 }
 
--- Sources/PopupBridge/UIApplication+URLOpener.swift
+++ Sources/PopupBridge/UIApplication+URLOpener.swift
@@ -1,17 +1,31 @@
 import UIKit
 
 protocol URLOpener {
-    
+
     func isVenmoAppInstalled() -> Bool
+    func isPayPalAppInstalled() -> Bool
+    func openURL(_ url: URL, completionHandler: @escaping (Bool) -> Void)
 }
 
 extension UIApplication: URLOpener {
-    
+
     /// Indicates whether the Venmo App is installed.
     func isVenmoAppInstalled() -> Bool {
         guard let venmoURL = URL(string: "com.venmo.touch.v2://") else {
             return false
         }
         return canOpenURL(venmoURL)
+    }
+
+    /// Indicates whether the PayPal App is installed.
+    func isPayPalAppInstalled() -> Bool {
+        guard let paypalURL = URL(string: "paypal-app-switch-checkout://") else {
+            return false
+        }
+        return canOpenURL(paypalURL)
+    }
+
+    func openURL(_ url: URL, completionHandler: @escaping (Bool) -> Void) {
+        open(url, options: [:], completionHandler: completionHandler)
     }
 }
