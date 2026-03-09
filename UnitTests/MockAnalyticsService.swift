22231790a17c1d82c3474156533d067a1eee58b9
Tanya Jain <tanyjain@paypal.com>
2026-03-09 16:47:40 -0500

    Add tests

--- UnitTests/MockAnalyticsService.swift
+++ UnitTests/MockAnalyticsService.swift
@@ -6,10 +6,12 @@ class MockAnalyticsService: AnalyticsServiceable {
     var lastEventName: String?
     var lastSessionID: String?
     var eventCount = 0
+    var allEventNames: [String] = []
 
     func sendAnalyticsEvent(_ eventName: String, sessionID: String) {
         lastEventName = eventName
         lastSessionID = sessionID
         eventCount += 1
+        allEventNames.append(eventName)
     }
 }
