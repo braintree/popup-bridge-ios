c6f636c12b665f5ecf085e66f5a2fd31c8b4a81d
Tanya Jain <tanyjain@paypal.com>
2026-03-23 12:08:57 -0500

    Clean up white spaces

--- Sources/PopupBridge/Analytics/PopupBridgeAnalytics.swift
+++ Sources/PopupBridge/Analytics/PopupBridgeAnalytics.swift
@@ -3,7 +3,6 @@ enum PopupBridgeAnalytics {
     static let succeeded = "popup-bridge:succeeded"
     static let failed = "popup-bridge:failed"
     static let canceled = "popup-bridge:canceled"
-
     static let paypalInstalled = "popup-bridge:app-detection:paypal-installed"
     static let paypalNotInstalled = "popup-bridge:app-detection:paypal-not-installed"
     static let appLaunchStarted = "popup-bridge:app-launch:started"
