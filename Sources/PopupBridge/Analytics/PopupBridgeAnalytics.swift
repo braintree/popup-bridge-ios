90edc47241af7418d26ed38dba117380fb26a008
joaaraujo-paypal <joaaraujo@paypal.com>
2026-06-08 12:12:11 -0300

    Update Sources/PopupBridge/Analytics/PopupBridgeAnalytics.swift
    Co-authored-by: Jax DesMarais-Leder <jdesmarais@paypal.com>

--- Sources/PopupBridge/Analytics/PopupBridgeAnalytics.swift
+++ Sources/PopupBridge/Analytics/PopupBridgeAnalytics.swift
@@ -3,7 +3,7 @@ enum PopupBridgeAnalytics {
     static let succeeded = "popup-bridge:succeeded"
     static let failed = "popup-bridge:failed"
     static let canceled = "popup-bridge:canceled"
-    static let appLaunchStarted = "popup-bridge:app-launch:started"
-    static let appLaunchSucceeded = "popup-bridge:app-launch:succeeded"
-    static let appLaunchFailed = "popup-bridge:app-launch:failed"
+    static let appLaunchStarted = "popup-bridge:app-switch:started"
+    static let appLaunchSucceeded = "popup-bridge:app-switch:succeeded"
+    static let appLaunchFailed = "popup-bridge:app-switch:failed"
 }
